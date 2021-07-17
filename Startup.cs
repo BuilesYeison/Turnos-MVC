1c83d307be56ae151ab712e4a7f21651abaf103a
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-07-17 13:13:45 -0500

    Se creo la base de datos a través de migración y confirmación de este. Se instalaron nuevas instancias de entityframework, cadena de conexion establecida.

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Turnos.Models;
 
 namespace Turnos
 {
@@ -24,6 +26,7 @@ namespace Turnos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<TurnosContext>(opciones => opciones.UseSqlServer(Configuration.GetConnectionString("TurnosContext"))); //agregamos el serivico entityframework y especificamos el servicio sql y su conexión que está en appsettings.json
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
