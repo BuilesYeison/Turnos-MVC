0806ba11b9db5a62a4bf264fda96808e4654fe0a
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-07-17 13:49:58 -0500

    Actualizamos el controlado especialidad y referenciamos en la vista index el modelo especialidad para mostrar informacion al usuario a traves de razor

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -1,22 +1,25 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Turnos.Models;
 
 namespace Turnos.Controllers
 {
     ///<remark> Controlador que se conecta con la vista y el modelo y adminsitra datos y se encarga de logica
     public class EspecialidadController : Controller
     {
+        private readonly TurnosContext _context;//solo lectura conexto del controlador
         ///<summary>
         ///Constructor de la clase
         ///</summary>
-        public EspecialidadController(){
-
+        public EspecialidadController(TurnosContext context){
+            _context = context; //cuando instaciamos esté controlador enviamos un context y lo inicializamos en _context
         }
 
         ///<summary>
         ///mostrar el resultado que obtiene este metodo en la interfaz del usuario Index
         ///</summary>
         public IActionResult Index(){
-            return View();
+            return View(_context.Especialidad.ToList()); //le pasamos a la vista una lista de los datos de la tabla Especialidad
         }
     }
 }
