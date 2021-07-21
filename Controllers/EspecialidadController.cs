e7a57d89d644b53ffa9e76cdccfdecdfdeefa550
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-07-20 20:28:52 -0500

    Se creó la funcionalidad de editar un registro de la tabla especialidad en la base de datos

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -22,5 +22,31 @@ namespace Turnos.Controllers
         public IActionResult Index(){
             return View(_context.Especialidad.ToList()); //le pasamos a la vista una lista de los datos de la tabla Especialidad
         }
+
+        public IActionResult Edit(int? id){//int? permite valores nulos
+            if(id == null){
+                return NotFound();
+            }
+            var especialidad = _context.Especialidad.Find(id); //objeto que permite obtener este dato de la base de datos según el id del registro que se paso 
+            if(especialidad == null){//no se encontró en la db el registro
+                return NotFound();
+            }
+            return View(especialidad);
+        }
+        ///<summary>
+        ///Se recibe la descripción y el id del registro en la base de datos
+        ///</summary>
+        [HttpPost]//Esto diferencia el metodo Edit que graba, del Edit de vista
+        public IActionResult Edit(int id, [Bind("IdEspecialidad, descripción")] Especialidad especialidad){
+            if(id != especialidad.IdEspecialidad){
+                return NotFound();
+            }
+            if(ModelState.IsValid){
+                _context.Update(especialidad);//actualizamos datos nuevos
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index)); //retornamos a la vista principal
+            }
+            return View(); 
+        }
     }
 }
