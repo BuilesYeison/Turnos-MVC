a9bd6e4ed3b5f7985769f348a7490d232bde0d12
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-07-20 22:03:49 -0500

    Especificacion de la creacion de la tabla

--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -18,5 +18,19 @@ namespace Turnos.Models
         ///<param name="Especialidad">DbSet es una tabla, una entidad</param>
         ///</summary>
         public DbSet<Especialidad> Especialidad{get;set;}
+
+        ///<summary>
+        ///Funcion que permite especificar la creación de la tabla según el modelo en el codigo. Permite especificar las propiedades de las columnas
+        ///</summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Especialidad>(entidad =>{
+                entidad.ToTable("Especialidad"); //nombre de la tabla
+                entidad.HasKey(e => e.IdEspecialidad);
+                entidad.Property(e => e.descripción)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            });
+        }
     }
 }
