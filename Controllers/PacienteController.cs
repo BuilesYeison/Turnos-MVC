8c294d7514825022d073a298634b51a67c6c86c7
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-07-25 10:56:12 -0500

    Nuevo modelo Paciente, se creo el controlador y los metodos para ver los detalles y crear nuevo paciente en la tabla

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Turnos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Turnos.Controllers{
+    public class PacienteController : Controller{
+        private readonly TurnosContext _context; 
+        public PacienteController(TurnosContext context){
+            _context = context;
+        }
+        ///<summary>
+        ///Pagina principal de MVC Paciente
+        ///</summary>
+        public async Task<IActionResult> Index(){
+            return View(await _context.Paciente.ToListAsync());
+        }
+
+        ///<summary>
+        ///Metodo que obtiene los detalles del paciente, retornamos la info del paciente solicitado a la vista
+        ///</summary>
+        public async Task<IActionResult> Details(int? id){
+            if(id == null){
+                return NotFound();
+            }
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.idPaciente == id);
+            if(paciente == null){
+                return NotFound();
+            }
+            return View(paciente);
+        }
+
+        ///<summary>
+        ///Metodo get para obtener la vista crear
+        ///</summary>
+        public IActionResult Create(){
+            return View();
+        }
+
+        ///<summary>
+        ///Metodo Post para obtener la informacion nueva de un paciente y agregarla a la tabla en bd
+        ///<param name="paciente">objeto con la información del paciente</param>
+        ///</summary>
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("idPaciente, Nombre, Apellido, Direccion, Email, Telefono")] Paciente paciente){
+            if(ModelState.IsValid){
+                _context.Add(paciente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -20,6 +20,12 @@ namespace Turnos.Models
         public DbSet<Especialidad> Especialidad{get;set;}
 
         ///<summary>
+        ///Creamos el dbset que apunta a la tabla en sql
+        ///<param name="Paciente">DbSet es un tabla o entidad</param>
+        ///</summary>
+        public DbSet<Paciente> Paciente{get;set;}
+
+        ///<summary>
         ///Funcion que permite especificar la creación de la tabla según el modelo en el codigo. Permite especificar las propiedades de las columnas
         ///</summary>
         protected override void OnModelCreating(ModelBuilder modelBuilder){
@@ -29,6 +35,30 @@ namespace Turnos.Models
                 entidad.Property(e => e.descripción)
                 .IsRequired()
                 .HasMaxLength(200)
+                .IsUnicode(false);
+            });
+            modelBuilder.Entity<Paciente>(entidad =>{
+                entidad.ToTable("Paciente");
+                entidad.HasKey(e => e.idPaciente);
+                entidad.Property(e => e.Nombre)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+                entidad.Property(e => e.Apellido)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+                entidad.Property(e => e.Direccion)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+                entidad.Property(e => e.Telefono)
+                .IsRequired()
+                .HasMaxLength(20)
+                .IsUnicode(false);
+                entidad.Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(100)
                 .IsUnicode(false);
             });
         }
